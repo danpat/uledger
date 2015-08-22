976f2aaf92b7bc6e42f651dd424dd02d3cfdc6da
Daniel Patterson <danpat@danpat.net>
2015-08-22 11:47:15 -0700

    Add some more tests (precision, tab parsing). Use \w instead of raw space, so we can support tabs. Move code into a class for easier re-use.

--- test.py
+++ test.py
@@ -3,24 +3,153 @@
 import unittest
 import uledger
 import textwrap
+import decimal
+
+class LedgerTest(unittest.TestCase):
+    def setUp(self):
+        self.ledger = uledger.Ledger()
 
 
-class Parsing(unittest.TestCase):
+class Parsing(LedgerTest):
 
     def test_baddata1(self):
-        data = """
+        data = textwrap.dedent("""
         Line1
             Line2
             Line3
         """
+        )
 
         with self.assertRaises(uledger.ParseError):
-            uledger.parse(data.splitlines(),"TESTDATA")
+            self.ledger.parse(data.splitlines(),"TESTDATA")
 
-    def test_ok(self):
+    def test_noposts(self):
         data = """2015-06-01 Dummy Transaction"""
         with self.assertRaises(uledger.ParseError):
-            uledger.parse(data.splitlines(),"TESTDATA")
+            self.ledger.parse(data.splitlines(),"TESTDATA")
+
+    def test_nocommodity(self):
+        data = """2015-01-01 Dummy
+            Source    50
+            Dest"""
+        with self.assertRaises(uledger.ParseError):
+            self.ledger.parse(data.splitlines(), "TESTDATA")
+
+    def test_spacing(self):
+        data = """2015-06-01\tDuummy transaction
+        \tSrc\t\t$1234
+        \tDest"""
+        self.ledger.parse(data.splitlines(),"TESTDATA")
+
+class Math(LedgerTest):
+
+    def test_basic(self):
+        data = textwrap.dedent("""
+        2015-01-01 Test
+            SourceAccount    $50
+            DestAccount""")
+
+        self.ledger.parse(data.splitlines(),"TESTDATA")
+        balance = self.ledger.balance("SourceAccount")
+        self.assertEquals(balance, {"$": 50 })
+        balance = self.ledger.balance("DestAccount")
+        self.assertEquals(balance, {"$": -50 })
+
+        with self.assertRaises(uledger.AccountNotFoundError):
+            balance = self.ledger.balance("SourceAccount2")
+
+    def test_precision1(self):
+        data = """2015-01-01 Dummy
+        Source  ($3.20 * 1.06)
+        Dest"""
+
+        self.ledger.parse(data.splitlines(),"TESTDATA")
+        balance = self.ledger.balance("Source")
+        self.assertEquals(balance, {"$": decimal.Decimal("3.39")})
+
+    def test_precision2(self):
+        data = textwrap.dedent("""
+        2015-01-01 Test
+            Source1    ($3.20 * 1.0609)
+            Dest
+            
+        2015-01-02 Test2
+            Source2    ($3.20 * 1.0610) 
+            Dest""")
+
+        # calculated value should be 3.3952, which should round up because the 3rd digit is >=5
+        self.ledger.parse(data.splitlines(),"TESTDATA")
+        balance = self.ledger.balance("Source1")
+        self.assertEquals(balance, {"$": decimal.Decimal("3.39")})
+        balance = self.ledger.balance("Source2")
+        self.assertEquals(balance, {"$": decimal.Decimal("3.40")})
+
+        balance = self.ledger.balance("Dest")
+        self.assertEquals(balance, {"$": decimal.Decimal("-6.79")})
+
+
+    def test_nobalance(self):
+        data = textwrap.dedent("""
+        2015-01-01 Test
+            SourceAccount    $50
+            DestAccount      $40""")
+
+        with self.assertRaises(uledger.ParseError):
+            self.ledger.parse(data.splitlines(),"TESTDATA")
+
+
+    def test_multipleposts(self):
+        data = textwrap.dedent("""
+        2015-01-01 Test
+            SourceAccount    $50
+            SourceAccount2    $50
+            SourceAccount3    $-50
+            DestAccount""")
+
+        self.ledger.parse(data.splitlines(),"TESTDATA")
+        balance = self.ledger.balance("SourceAccount")
+        self.assertEquals(balance, {"$": 50 })
+        balance = self.ledger.balance("DestAccount")
+        self.assertEquals(balance, {"$": -50 })
+        balance = self.ledger.balance("SourceAccount2")
+        self.assertEquals(balance, {"$": 50 })
+        balance = self.ledger.balance("SourceAccount3")
+        self.assertEquals(balance, {"$": -50 })
+
+    def test_multipletransactions(self):
+        data = textwrap.dedent("""
+        2015-01-01 Test
+            SourceAccount    $50
+            SourceAccount2    $50
+            SourceAccount3    $-50
+            DestAccount
+            
+        2015-01-02 Test2
+            SourceAccount     $25
+            DestAccount""")
+
+        self.ledger.parse(data.splitlines(),"TESTDATA")
+        balance = self.ledger.balance("SourceAccount")
+        self.assertEquals(balance, {"$": 75 })
+        balance = self.ledger.balance("DestAccount")
+        self.assertEquals(balance, {"$": -75 })
+        balance = self.ledger.balance("SourceAccount2")
+        self.assertEquals(balance, {"$": 50 })
+        balance = self.ledger.balance("SourceAccount3")
+        self.assertEquals(balance, {"$": -50 })
+
+    def test_multiplecommodities(self):
+        data = textwrap.dedent("""
+        2015-01-01 Test
+            SourceAccount    $50
+            SourceAccount    50 CAD
+            DestAccount""")
+
+        self.ledger.parse(data.splitlines(),"TESTDATA")
+        balance = self.ledger.balance("SourceAccount")
+        self.assertEquals(balance, {"$": 50, "CAD": 50 })
+        balance = self.ledger.balance("DestAccount")
+        self.assertEquals(balance, {"$": -50, "CAD": -50 })
 
 
 if __name__ == '__main__':
--- uledger.py
+++ uledger.py
@@ -18,183 +18,193 @@ class ParseError(Exception):
     def __str__(self):
         return "ERROR: %s:%s: %s" % (self.filename, self.linenum, self.msg)
 
+class AccountNotFoundError(Exception):
+    def __init__(self, account):
+        self.account = account
+    def __str__(self):
+        return "ERROR: Account '%s' not found" % (self.account)
 
-# This is a dict of dates
-#   each member is a transaction, sorted by parse order
-transactions = {}
-accounts = {}
-aliases = {}
-commodities = set()
 
-def parseamount(amountstr):
-    m = re.match("\((.*?)\)",amountstr)
-    if m:
-        # $1234.12 + $123432.23
-        m = re.match("\((.*?) +\+ +(.*?)\)",amountstr)
+class Ledger(object):
+
+    # This is a dict of dates
+    #   each member is a transaction, sorted by parse order
+    transactions = {}
+    accounts = {}
+    aliases = {}
+    commodities = set()
+
+    def __init__(self):
+        self.transactions = {}
+        self.accounts = {}
+        self.aliases = {}
+        self.commodities = set()
+
+    def parseamount(self, amountstr, filename, linenum):
+        m = re.match("\((.*?)\)",amountstr)
         if m:
-            a = parseamount(m.groups()[0])
-            b = parseamount(m.groups()[1])
-            return Amount(a.commodity,a.value+b.value)
-
-        m = re.match("\((.*?) +\* +(-?\d+(\.\d+)?) *\)",amountstr)
+            # $1234.12 + $123432.23
+            m = re.match("\(\s*(.*?)\s+\+\s+(.*?)\s*\)",amountstr)
+            if m:
+                a = self.parseamount(m.groups()[0],filename,linenum)
+                b = self.parseamount(m.groups()[1],filename,linenum)
+                return Amount(a.commodity,a.value+b.value)
+    
+            m = re.match("\(\s*(.*?)\s+\*\s+(-?\d+(\.\d+)?)\s*\)",amountstr)
+            if m:
+                a = self.parseamount(m.groups()[0],filename,linenum)
+                b = decimal.Decimal(m.groups()[1])
+                return Amount(a.commodity,(a.value*b).quantize(decimal.Decimal('.01'), rounding=decimal.ROUND_HALF_UP))
+    
+        # $-1234.34
+        m = re.match("(\$)\s*(-?\d+(\.\d+)?)",amountstr)
         if m:
-            a = parseamount(m.groups()[0])
-            b = decimal.Decimal(m.groups()[1])
-            return Amount(a.commodity,(a.value*b).quantize(decimal.Decimal('.01'), rounding=decimal.ROUND_HALF_UP))
+            return Amount(m.groups()[0],decimal.Decimal(m.groups()[1]))
+    
+        # -123.43 CAD
+        m = re.match("(-?\d+(\.\d+)?) (\w+)",amountstr)
+        if m:
+            return Amount(m.groups()[-1],decimal.Decimal(m.groups()[0]))
 
-    # $-1234.34
-    m = re.match("(\$) *(-?\d+(\.\d+)?)",amountstr)
-    if m:
-        return Amount(m.groups()[0],decimal.Decimal(m.groups()[1]))
+        raise ParseError(filename, linenum, "Don't know how to interpret '%s' as a value, did you include a commodity type ($, USD, etc)?" % amountstr)
+        return None
+    
+    def makepost(self, account,date,commodity,amount):
+        self.commodities.add(commodity)
+        if account not in self.accounts:
+            self.accounts[account] = {}
+        if date not in self.accounts[account]:
+            self.accounts[account][date] = {}
+        if commodity not in self.accounts[account][date]:
+            self.accounts[account][date][commodity] = []
+        self.accounts[account][date][commodity].append(amount)
+    
+    
+    # We lexically sort the date keys, and start from
+    # the beginning to get the current balance
+    def balance(self, account,asof=None):
 
-    # -123.43 CAD
-    m = re.match("(-?\d+(\.\d+)?) (\w+)",amountstr)
-    if m:
-        return Amount(m.groups()[-1],decimal.Decimal(m.groups()[0]))
-
-def makepost(account,date,commodity,amount):
-
-    commodities.add(commodity)
-
-    if account not in accounts:
-        accounts[account] = {}
-
-    if date not in accounts[account]:
-        accounts[account][date] = {}
-
-    if commodity not in accounts[account][date]:
-        accounts[account][date][commodity] = []
-
-    accounts[account][date][commodity].append(amount)
-
-
-# We lexically sort the date keys, and start from
-# the beginning to get the current balance
-def balance(account,asof=None):
-
-    balances = {}
-    datekeys = accounts[account].keys()
-    datekeys.sort()
-    for date in datekeys:
-        # We assumd 2015-02-32 which will compare lexically
-        if asof is None or date < asof:
-            for commodity in accounts[account][date]:
-                if commodity not in balances:
-                    balances[commodity] = 0
-                balances[commodity] += sum(accounts[account][date][commodity])
-        else:
-            break
-    return balances
-
-def maketransaction(transaction, posts, bucket = None):
-    balanceaccount = bucket
-    amounts = {}
-    if len(posts) == 0 or len(posts) == 1 and posts[0].amount.commodity is None:
-        raise ParseError(transaction.filename, transaction.linenum, "No transactions")
-
-    for post in posts:
-        account = post.account
-        if account in aliases:
-            account = aliases[post.account]
-        if post.amount is None or post.amount.value is None:
-            if balanceaccount is None or balanceaccount == bucket:
-                balanceaccount = account
+        if account not in self.accounts:
+            raise AccountNotFoundError(account)
+    
+        balances = {}
+        datekeys = self.accounts[account].keys()
+        datekeys.sort()
+        for date in datekeys:
+            # We assumd 2015-02-32 which will compare lexically
+            if asof is None or date < asof:
+                for commodity in self.accounts[account][date]:
+                    if commodity not in balances:
+                        balances[commodity] = 0
+                    balances[commodity] += sum(self.accounts[account][date][commodity])
             else:
-                raise ParseError(post.filename, post.linenum, "Cannot have multiple empty posts")
-        else:
-            if post.amount is None:
-                raise ParseError(post.filename, post.linenum, "Could not understand \"%s\"" % p[2])
-            if post.amount.commodity not in amounts:
-                amounts[post.amount.commodity] = 0
+                break
+        return balances
 
-            amounts[post.amount.commodity] += post.amount.value
-
-            makepost(account, transaction.date, post.amount.commodity, post.amount.value)
-            #print (transaction[0],) + (acct,) + amount
-
-    for commodity in amounts:
-        if amounts[commodity] != decimal.Decimal("0"):
-            if balanceaccount is not None:
-                #print "Balancing post:", (transaction[0],) + (balanceacct,) + (key, amounts[key]*-1)
-                makepost(balanceaccount, transaction.date, commodity, -amounts[commodity])
+    def maketransaction(self, transaction, posts, bucket = None):
+        balanceaccount = bucket
+        amounts = {}
+        if len(posts) == 0 or len(posts) == 1 and posts[0].amount.commodity is None:
+            raise ParseError(transaction.filename, transaction.linenum, "No transactions")
+    
+        for post in posts:
+            account = post.account
+            if account in self.aliases:
+                account = self.aliases[post.account]
+            if post.amount is None or post.amount.value is None:
+                if balanceaccount is None or balanceaccount == bucket:
+                    balanceaccount = account
+                else:
+                    raise ParseError(post.filename, post.linenum, "Cannot have multiple empty posts")
             else:
-                raise ParseError(post.filename, post.linenum, "Transaction does not balance: %f %s outstanding" % (amounts[commodity], commodity))
+                if post.amount is None:
+                    raise ParseError(post.filename, post.linenum, "Could not understand \"%s\"" % p[2])
+                if post.amount.commodity not in amounts:
+                    amounts[post.amount.commodity] = 0
+    
+                amounts[post.amount.commodity] += post.amount.value
+    
+                self.makepost(account, transaction.date, post.amount.commodity, post.amount.value)
+    
+        for commodity in amounts:
+            if amounts[commodity] != decimal.Decimal("0"):
+                if balanceaccount is not None:
+                    self.makepost(balanceaccount, transaction.date, commodity, -amounts[commodity])
+                else:
+                    raise ParseError(post.filename, post.linenum, "Transaction does not balance: %f %s outstanding" % (amounts[commodity], commodity))
+    
+    # Parses a file, can be called recursively
+    def parse(self, reader,filename=None):
+    
+        bucket = None
+        transaction = None
+        accountdef = None
+        posts = []
+        for linenum, line in enumerate(reader):
+    
+            line = line.rstrip()
+            m = re.match(" *;", line)
+            if line == '' or m:
+                continue
+    
 
-# Parses a file, can be called recursively
-def parse(reader,filename=None):
-
-    bucket = None
-    transaction = None
-    accountdef = None
-    posts = []
-    for linenum, line in enumerate(reader):
-
-        line = line.rstrip()
-        # print "#%s" % line
-        m = re.match(" *;", line)
-        if line == '' or m:
-            continue
-
-
+            if transaction is not None:
+                m = re.match("^\s+(.*?)(\s\s+(.*))?$", line)
+                if m:
+                    amount = None
+                    if m.groups()[2] is not None:
+                        amount = self.parseamount(m.groups()[2],filename,linenum)
+                    post = Post(m.groups()[0],amount,filename,linenum)
+                    posts.append(post)
+                    continue
+                else:
+                    self.maketransaction(transaction, posts, bucket)
+                    posts = []
+                    transaction = None
+    
+            if accountdef is not None:
+                m = re.match("^\s+(.*)$",line)
+                if m:
+                    continue
+                else:
+                    accountdef = None
+    
+            m = re.match("(\d{4}-\d{2}-\d{2})(=\d{4}-\d{2}-\d{2})?\s+(.*)", line)
+            if m:
+                transaction = Transaction(m.groups()[0],m.groups()[1],filename,linenum)
+                continue
+    
+            m = re.match("commodity\s+(.*)", line)
+            if m:
+                continue
+    
+            m = re.match("account\s+(.*)", line)
+            if m:
+                accountdef = m.groups()
+                continue
+    
+            m = re.match("include\s+(.*)",line)
+            if m:
+                includefile = m.groups()[0]
+                with open(includefile) as f:
+                    self.parse(f,includefile)
+                continue
+    
+            m = re.match("bucket\s+(.*)",line)
+            if m:
+                bucket = m.groups()[0]
+                continue
+    
+            m = re.match("alias\s+(.*?)\s+(.*)",line)
+            if m:
+                self.aliases[m.groups()[0]] = m.groups()[1]
+                continue
+    
+            raise ParseError(filename, linenum, "Don't know how to process \"%s\"" % line)
+    
         if transaction is not None:
-            m = re.match("^ +(\w.+?)(  +(.*))?$", line)
-            if m:
-                print m.groups()
-                amount = None
-                if m.groups()[2] is not None:
-                    amount = parseamount(m.groups()[2])
-                post = Post(m.groups()[0],amount,filename,linenum)
-                posts.append(post)
-                continue
-            else:
-                maketransaction(transaction, posts, bucket)
-                posts = []
-                transaction = None
-
-        if accountdef is not None:
-            m = re.match("^ +(.*)$",line)
-            if m:
-                continue
-            else:
-                accountdef = None
-
-        m = re.match("(\d{4}-\d{2}-\d{2})(=\d{4}-\d{2}-\d{2})? +(.*)", line)
-        if m:
-            transaction = Transaction(m.groups()[0],m.groups()[1],filename,linenum)
-            continue
-
-        m = re.match("commodity +(.*)", line)
-        if m:
-            continue
-
-        m = re.match("account +(.*)", line)
-        if m:
-            accountdef = m.groups()
-            continue
-
-        m = re.match("include (.*)",line)
-        if m:
-            includefile = m.groups()[0]
-            with open(includefile) as f:
-                parse(f,includefile)
-            continue
-
-        m = re.match("bucket (.*)",line)
-        if m:
-            bucket = m.groups()[0]
-            continue
-
-        m = re.match("alias +(.*?) +(.*)",line)
-        if m:
-            aliases[m.groups()[0]] = m.groups()[1]
-            continue
-
-        raise ParseError(filename, linenum, "Don't know how to process \"%s\"" % line)
-
-    if transaction is not None:
-        maketransaction(transaction,posts,bucket)
-
-
+            self.maketransaction(transaction,posts,bucket)
+    
 
 if __name__ == "__main__":
 
@@ -205,11 +215,12 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
+    ledger = Ledger()
     with open(args.filename) as f:
-        parse(f,args.filename)
+        ledger.parse(f,args.filename)
 
     if args.command == "balance":
-        accountkeys = accounts.keys()
+        accountkeys = ledger.accounts.keys()
         accountkeys.sort()
 
         maxlen = 0
@@ -217,15 +228,15 @@ if __name__ == "__main__":
             maxlen = max(maxlen,len(account))
 
         print "Account".ljust(maxlen+1," "),
-        for commodity in commodities:
+        for commodity in ledger.commodities:
             print commodity.rjust(10," "),
         print
 
-        print "-" * (maxlen+1 + len(commodities)*11)
+        print "-" * (maxlen+1 + len(ledger.commodities)*11)
         for account in accountkeys:
             #print ":".join(reversed(account.split(":"))).rjust(maxlen+1," "),
             print account.ljust(maxlen+1,"."),
-            b = balance(account)
+            b = ledger.balance(account)
             for i, commodity in enumerate(b):
                 if commodity in b:
                     print str(b[commodity]).rjust(10," "),
